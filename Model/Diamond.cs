0c42d2847d589c36baddcaa992daae1c83cae34b
MHozaifaA <botchey.2012@gmail.com>
2020-12-01 23:11:06 +0200

    UI and dataset https://github.com/Pratik-Bhujade/Diamond-Dataset/blob/master/DiamondData.csv

---
+++ Model/Diamond.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiamondPricePrediction.Model
+{
+    public class Diamond
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+    }
+}
